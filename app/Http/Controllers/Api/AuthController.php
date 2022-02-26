cb2824ab3c74899817ca8956c25c100c750e496d
Sovannak22 <38850319+Sovannak22@users.noreply.github.com>
2022-02-26 22:44:46 +0700

    implement login

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $loginData = $request->validate([
+            'phone_number' => 'required',
+            'password' => 'required'
+        ]);
+        $user = User::where('phone_number', $request->phone_number)->first();
+        if ($user) {
+            if (Hash::check($request->password, $user->password)) {
+                $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+                $response = ['token' => $token['token'], 'user' => $user];
+                return response($response, 200);
+            } else {
+                $response = ["message" => "Password mismatch"];
+                return response($response, 422);
+            }
+        } else {
+            $response = ["message" =>'User does not exist'];
+            return response($response, 422);
+        }
+
+    }
+}
